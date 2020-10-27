16ed48c3105f58a301858da5b63bc491df7ef548
Keith Brandenburg <kab2003@gmail.com>
2020-10-27 01:47:26 -0500

    stuff + testing push to remote on commit

--- commandLine1/main.swift
+++ commandLine1/main.swift
@@ -35,7 +35,7 @@ var x = 0
 while x < 100000 {
     print(x)
     x = x + 100
-}*/
+}
 
 var total = 0
 for i in 0..<4 {
@@ -44,4 +44,50 @@ for i in 0..<4 {
     print("adding \(i) to \(oldTotal) to become \(total)")
 }
 print(total)
+
+
+var mytuple = [Int]() //define empty int array
+//you cannot use this syntax
+//mytuple[0]=50
+mytuple.insert(50, at: 0)
+
+print(mytuple[0])*/
+
+var optionalString: String? = "Hello"
+//print(optionalString == nil)
+// Prints "false"
+
+var optionalName: String? = nil
+var greeting = "Hello!"
+if let name = optionalName {
+    greeting = "Hello, \(name)"
+}
+//print(greeting)
+
+
+/* interesting demo of how prefix works -
+    actually not too interesting since there
+    ended up being no difference */
+prefix func ++( x: inout Int) -> Int {
+    let current = x
+    x += 1
+    return current
+}
+postfix func ++( x: inout Int) -> Int {
+    let current = x
+    x += 1
+    return current
+}
+
+//var xx = 1
+//var y = 1
+//print(xx++)
+//print(xx)
+//print(++y)
+//print(y)
+//var z = ++y // if this was a postfix z would equal y, nvm y=2 z=1
+//print("y=\(y)\nz=\(z)")
+//
+//print("in prefix z should equal y wtf")
+print("blah")
 
